c6b1f1d3794fd14f9e637628735763dab47bded5
Sadrazambot <Sadrazambot@gmail.com>
2026-06-03 17:40:36 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,15 @@
-print("Sadrazam Bot Started")
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("سلام 👋 من ربات صدر اعظم هستم")
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+
+print("Bot is running...")
+app.run_polling()
