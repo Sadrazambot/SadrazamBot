dcaa1ac417fe808b6cfb6d97f79c72913beeb725
Sadrazambot <Sadrazambot@gmail.com>
2026-06-03 18:35:54 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,16 +7,8 @@ TOKEN = os.getenv("BOT_TOKEN")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("ربات فعال شد 👋")
 
-def main():
-    if not TOKEN:
-        print("BOT_TOKEN is missing!")
-        return
+app = ApplicationBuilder().token(TOKEN).build()
+app.add_handler(CommandHandler("start", start))
 
-    app = ApplicationBuilder().token(TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-
-    print("Bot started")
-    app.run_polling()
-
-if __name__ == "__main__":
-    main()
+print("Bot is running...")
+app.run_polling()
